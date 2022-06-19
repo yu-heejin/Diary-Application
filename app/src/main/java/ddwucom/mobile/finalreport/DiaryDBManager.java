fdb633f6c990b0cd8669b274493a97b58d8f9f24
hee <jkgt0521@naver.com>
2022-06-19 17:45:55 +0900

    android

--- app/src/main/java/ddwucom/mobile/finalreport/DiaryDBManager.java
+++ app/src/main/java/ddwucom/mobile/finalreport/DiaryDBManager.java
@@ -2,6 +2,10 @@ package ddwucom.mobile.finalreport;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class DiaryDBManager {
     DiaryDBHelper diaryDBHelper = null;
@@ -14,7 +18,25 @@ public class DiaryDBManager {
     //Create
 
     //Read
+    public ArrayList<Diary> getAllDiary() {
+        ArrayList<Diary> diaryArrayList = new ArrayList<>();
+        SQLiteDatabase db = diaryDBHelper.getReadableDatabase();
 
+        Cursor cursor = db.rawQuery("SELECT * FROM " + diaryDBHelper.TABLE_NAME, null);
+
+        while (cursor.moveToNext()) {
+            long id = cursor.getInt(0);
+            String title = cursor.getString(1);
+            String feeling = cursor.getString(2);
+            String weather = cursor.getString(3);
+            String date = cursor.getString(4);
+            String detail = cursor.getString(5);
+            int picture = cursor.getInt(6);
+        }
+
+        cursor.close();
+
+    }
     //Update
 
     //Delete
